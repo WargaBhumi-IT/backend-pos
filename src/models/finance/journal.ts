cbe4ff3a5557dfce64297958228951d5247b5cc0
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2022-12-22 02:41:50 +0700

    finished journal and ledger model and controller

--- src/models/finance/journal.ts
+++ src/models/finance/journal.ts
@@ -1,12 +1,22 @@
 import { Schema, model } from "mongoose";
 
 
-export interface IJournal {
-    date: Date,
-    description: string
+export interface IJournalEntry {
+    id: string,
+    description: string,
+    account: string,
+    debit: number,
+    credit: number,
 }
 
-export const JournalSchema = new Schema({
+export interface IJournal {
+    date: Date,
+    description: string;
+    entries: Array<IJournalEntry>;
+    last_edited: Date;
+}
+
+export const JournalSchema = new Schema<IJournal>({
     date: {
         type: Date,
         required: true,
@@ -16,22 +26,163 @@ export const JournalSchema = new Schema({
         type: String,
         required: true,
         default: ""
+    },
+    entries: {
+        type: [{
+            id: {
+                type: Schema.Types.ObjectId,
+                required: true,
+                auto: true
+            },
+            description: {
+                type: String,
+                required: true
+            },
+            account: {
+                type: String,
+                required: true
+            },
+            debit: {
+                type: Number,
+                required: true
+            },
+            credit: {
+                type: Number,
+                required: true
+            },
+        }],
+        required: true,
+        default: []
+    },
+    last_edited: {
+        type: Date,
+        required: true,
+        default: () => new Date()
     }
 });
 
-class Journal implements IJournal {
-    public id: string;
-    public date: Date;
-    public description: string;
-
-    private constructor(id: string, date: Date, description: string) {
-        this.id = id;
-        this.date = date;
-        this.description = description;
-    }
-}
-
 
 export const JournalModel = model<IJournal>("finance.journal", JournalSchema);
+
+const __journal_memo = new Map<string, Journal>();
+class Journal implements IJournal {
+    private __is_deleted: boolean = false;
+
+    private _id: string;
+    private _date: Date;
+    private _description: string;
+    private _entries: Array<IJournalEntry>;
+    private _last_edited: Date;
+
+    private constructor(id: string, date: Date, description: string, entries: Array<IJournalEntry>, last_edited: Date) {
+        this._id = id;
+        this._date = date;
+        this._description = description;
+        this._entries = entries;
+        this._last_edited = last_edited;
+
+
+        __journal_memo.set(id, this);
+    }
+
+    public static async getById(id: string) {
+        const _j = __journal_memo.get(id); 
+        if (_j) return _j;
+
+        const j = await JournalModel.findById(id);
+        if (!j) throw new Error(`Journal By id ${id} not found`);
+
+        return new Journal(
+            j._id.toString(),
+            j.date,
+            j.description,
+            j.entries,
+            j.last_edited
+        )
+    }
+
+    public get id()             { if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`); return this._id; }
+    public get date()           { if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`); return this._date; }
+    public get description()    { if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`); return this._description; }
+    public get entries()        { if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`); return this._entries; }
+    public get last_edited()    { if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`); return this._last_edited; }
+    
+    public get objectified() {
+        const entries = new Array<IJournalEntry>();
+        for(const entry of entries) {
+            entries.push({
+                id: entry.id,
+                description: entry.description,
+                account: entry.account,
+                debit: entry.debit,
+                credit: entry.credit
+            })
+        }
+        return {
+            id: this._id,
+            date: this._date,
+            description: this._description,
+            last_edited: this._last_edited,
+            entries
+        }
+    }
+
+    public async update(date?: Date, description?: string) {
+        if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`);
+
+        let u_date = date ?? this.date;
+        let u_description = description ?? this.description;
+        
+        const j = await JournalModel.findByIdAndUpdate(this.id, { last_updated: new Date(), date: u_date, description: u_description }, { new: true });
+        if(!j) throw new Error(`Failed to add Entry Journal with id ${this.id}`);
+
+        this._id = j._id.toString();
+        this._date = j.date;
+        this._description = j.description;
+        this._entries = j.entries;
+        this._last_edited = j.last_edited
+
+        __journal_memo.set(this.id, this);
+        return this;
+    }
+
+    public async addEntry(description: string, account: string, debit: number, credit: number) {
+        if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`);
+
+        const entry = {
+            description,
+            account,
+            debit,
+            credit
+        }
+        const j = await JournalModel.findByIdAndUpdate(this.id, { last_updated: new Date(), $push: { entries: entry } }, { new: true });
+        if(!j) throw new Error(`Failed to add Entry Journal with id ${this.id}`);
+
+        this._id = j._id.toString();
+        this._date = j.date;
+        this._description = j.description;
+        this._entries = j.entries;
+        this._last_edited = j.last_edited
+
+        __journal_memo.set(this.id, this);
+        return this;
+    }
+    
+    public async deleteEntry(entry_id: string) {
+        if(this.__is_deleted) throw new Error(`Journal with id ${this.id} already deleted`);
+
+        const j = await JournalModel.findByIdAndUpdate(this.id, { last_updated: new Date(), $pull: { entries: { id: entry_id } }}, { new: true });
+        if(!j) throw new Error(`Failed to add Entry Journal with id ${this.id}`);
+
+        this._id = j._id.toString();
+        this._date = j.date;
+        this._description = j.description;
+        this._entries = j.entries;
+        this._last_edited = j.last_edited;
+
+        __journal_memo.set(this.id, this);
+        return this;
+    }
+}
 
 export default Journal;
--- src/models/finance/ledger.ts
+++ src/models/finance/ledger.ts
@@ -1,12 +1,23 @@
 import { Schema, model } from "mongoose";
-
+import Journal from "./journal";
+import crypto from "crypto";
+import * as DEBUG from "../../debug";
 
 export interface ILedger {
-    closed_date: Date,
-    journals: Array<string> // Array of Journal Id
+    index: number
+    start_date: Date
+    closing_date: Date
+    journals: Array<string>
+    nonce: number
+    previous_ledger_signature: string
 }
 
 export const LedgerSchema = new Schema({
+    index: {
+        type: Number,
+        required: true,
+        unique: true
+    },
     start_date: {
         type: Date,
         required: true
@@ -19,6 +30,203 @@ export const LedgerSchema = new Schema({
         type: [String],
         required: true,
         default: []
+    },
+    nonce: {
+        type: Number,
+        required: true
+    },
+    previous_ledger_signature: {
+        type: String,
+        required: true
     }
 });
 
+const LedgerModel = model<ILedger>("finance.ledger", LedgerSchema);
+
+class Ledger {
+    private _id: string;
+    private _index: number;
+    private _start_date: Date;
+    private _closing_date: Date;
+    private _journals: Array<Journal>;
+    private _nonce: number;
+    private _previous_ledger_signature: string;
+
+    private constructor(id: string, index: number, start_date: Date, closing_date: Date, journals: Array<Journal>, nonce: number, previous_ledger_signature: string) {
+        this._id = id;
+        this._index = index
+        this._start_date = start_date;
+        this._closing_date = closing_date;
+        this._journals = journals
+        this._nonce = nonce;
+        this._previous_ledger_signature = previous_ledger_signature;
+    }
+
+    public static async getLastIndex() {
+        const ledgers = await LedgerModel.find();
+        let last_ledger = ledgers[0];
+
+        for(const ledger of ledgers) {
+            if(ledger.index >= last_ledger.index) {
+                last_ledger = ledger;
+            }
+        }
+
+        const journals = new Array<Journal>();
+        for(const id of last_ledger.journals) {
+            const journal = await Journal.getById(id);
+            journals.push(journal);
+        }
+
+        return new Ledger(
+            last_ledger.id,
+            last_ledger.index,
+            last_ledger.start_date,
+            last_ledger.closing_date,
+            journals,
+            last_ledger.nonce,
+            last_ledger.previous_ledger_signature,
+        )
+    }
+
+    public static async newLedger(start_date: Date, closing_date: Date, journal_ids: Array<string>, nonce: number) {
+        const journals = new Array<Journal>();
+        for(const id of journal_ids) {
+            const journal = await Journal.getById(id);
+            journals.push(journal);
+        }
+
+        const previous_ledger = await Ledger.getLastIndex()
+        
+        return new Ledger(
+            crypto.createHash("sha512").update(crypto.randomBytes(512)).digest("hex"),
+            previous_ledger.index + 1,
+            start_date,
+            closing_date,
+            journals,
+            nonce,
+            previous_ledger.signature,
+        )
+    }
+
+    public static async getByIndex(target_index: number): Promise<Ledger> {
+        const ledger = await LedgerModel.findOne({ index: target_index });
+        if(!ledger) throw new Error(`ledger index of ${target_index} not found`);
+
+        const journals = new Array<Journal>();
+        for(const id of ledger.journals) {
+            const journal = await Journal.getById(id);
+            journals.push(journal);
+        }
+
+        return new Ledger(
+            ledger._id.toString(),
+            ledger.index,
+            ledger.start_date,
+            ledger.closing_date,
+            journals,
+            ledger.nonce,
+            ledger.previous_ledger_signature
+        )
+    }
+
+    public static async getById(target_id: string) {
+        const ledger = await LedgerModel.findById(target_id);
+        if(!ledger) throw new Error(`ledger id of ${target_id} not found`);
+
+        const journals = new Array<Journal>();
+        for(const id of ledger.journals) {
+            const journal = await Journal.getById(id);
+            journals.push(journal);
+        }
+
+        return new Ledger(
+            ledger._id.toString(),
+            ledger.index,
+            ledger.start_date,
+            ledger.closing_date,
+            journals,
+            ledger.nonce,
+            ledger.previous_ledger_signature
+        )
+    }
+
+    public get id() { return this._id; }
+    public get index() { return this._index; }
+    public get start_date() { return this._start_date; }
+    public get closing_date() { return this._closing_date; }
+    public get journals() { return this._journals; }
+    public get nonce() { return this._nonce; }
+    public get previous_ledger_signature() { return this._previous_ledger_signature; }
+
+    public get objectified() {
+        const parsed_journals = this._journals.map(journal => journal.objectified)
+        return {
+            id: this._id,
+            index: this._index,
+            start_date: this._start_date,
+            closing_date: this._closing_date,
+            journals: parsed_journals,
+            nonce: this._nonce,
+            previous_ledger_signature: this._previous_ledger_signature,
+        }
+    }
+
+    public trySignature(target_nonce: number) {
+        const parsed_journals = this._journals.map(journal => journal.objectified)
+        return crypto.createHash("sha512").update(JSON.stringify({
+            id: this._id,
+            index: this._index,
+            start_date: this._start_date,
+            closing_date: this._closing_date,
+            journals: parsed_journals,
+            nonce: target_nonce,
+            previous_ledger_signature: this._previous_ledger_signature,
+        })).digest("hex");
+    }
+
+    public trySign(target_nonce: number) {
+        return this.trySignature(target_nonce).slice(-4) == "0000";
+    }
+
+    public async sign(target_nonce: number) {
+        if(!this.trySign(target_nonce)) throw new Error(`failed to sign ledger id ${this._id}`);
+
+        this._nonce = target_nonce;
+        DEBUG.log(`Ledger Id ${this.id} mined with nonce ${target_nonce} and signature: ${this.signature}`);
+
+        (new LedgerModel({
+            _id: this._id,
+            index: this._index,
+            start_date: this._start_date,
+            closing_date: this._closing_date,
+            journals: this._journals,
+            nonce: this._nonce,
+            previous_ledger_signature: this._previous_ledger_signature,
+        }).save()).then(ledger => DEBUG.log(`Ledger Id ${ledger._id.toString()} Saved to Database`)).catch(() => DEBUG.error(`Failed to save Ledger Id ${this.id} to database`));
+    }
+
+    public get signature() {
+        return this.trySignature(this._nonce);
+    }
+
+    public get isValid() {
+        return this.signature.slice(-4) == "0000"
+    }
+}
+
+async function mineLedger(ledger: Ledger) {
+    const id = crypto.createHash("md5").update(crypto.randomBytes(128));
+
+    let nonce = 0;
+    DEBUG.log(`Mining ledger id ${ledger.id} with miner id ${id}`);
+    while(true) {
+        const status = ledger.trySign(nonce);
+        if(status) {
+            ledger.sign(nonce);
+            return;
+        }
+
+        nonce += 1;
+    }
+}
